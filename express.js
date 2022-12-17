1af8793d0c9497c0c30fdaee198007f16a1c3122
G <45158534+nattavutmail4@users.noreply.github.com>
2022-12-17 10:29:54 +0700

    Update

--- express.js
+++ express.js
@@ -1,11 +1,17 @@
 const { response } = require('express')
 const express =require('express') //common (cjs)
+const port = 8080
 const app = express()
+
 
 app.get('/',(request,response)=>{
   response.send('homepage')
 })
 
-app.listen(8080,()=>{
-  console.log(`Run server is url http://127.0.0.1:${8080}`)
+app.get('/ping',(request,response)=>{
+  response.send('pong')
+})
+
+app.listen(port,()=>{
+  console.log(`Run server is url http://127.0.0.1:${port}`)
 })
