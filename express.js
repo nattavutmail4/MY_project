01c2d32f03bc99d060946739a1f3ddd13e244625
Golf <45158534+nattavutmail4@users.noreply.github.com>
2022-12-17 11:41:42 +0700

    Add format respone

--- express.js
+++ express.js
@@ -1,13 +1,45 @@
 const { response } = require('express')
 const express =require('express') //common (cjs)
+
 const port = 8080
 const app = express()
 
 
+
+
 app.get('/',(req,res)=>{
-  res.send('homepage')
+  res.format({
+    'text/html':()=>{
+      res.send('<h1>Hello World</h1>')
+    },
+    'text/plain':()=>{
+      res.set('Content-Type','text/plain')
+      res.send('<h1>Hello World</h1>')
+    },
+    'application/json':()=>{
+      res.send({text:'hello world'})
+    },
+    default:()=>{
+      res.status(400).send('Content-Type not allow')
+    }
+  })
 })
 
+app.get('/dowload',(req,res)=>{
+   res.download('./img/christmas.jpg','chirstmas.png') //สำหรับดาวโหลดไฟล์ และเปลี่ยนนามสกุลไฟล์
+  // res.send('ok!')
+})
+app.get('/google',(req,res)=>{
+  res.redirect(301,'https://www.google.com') // redirect ไปยังหน้าอื่นๆ
+})
+
+app.get('/notfound',(req,res)=>{
+  res.status(404).send('notfound')
+})
+
+app.get('/notallow',(req,res)=>{
+  res.status('403').send('ไม่อนุญาติ')
+})
 app.get('/ping',(req,res)=>{
   res.send('pong')
 })
