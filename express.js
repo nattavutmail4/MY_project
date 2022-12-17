f4077f7065c2545984e0050978aed375b7ea4d5b
G <45158534+nattavutmail4@users.noreply.github.com>
2022-12-17 10:26:21 +0700

    Update

--- express.js
+++ express.js
@@ -1,6 +1,11 @@
+const { response } = require('express')
 const express =require('express') //common (cjs)
 const app = express()
 
 app.get('/',(request,response)=>{
+  response.send('homepage')
+})
 
+app.listen(8080,()=>{
+  console.log(`Run server is url http://127.0.0.1:${8080}`)
 })
