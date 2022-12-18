e9bd42348ce013dea3843b4750022a2c994e8fa0
Golf <45158534+nattavutmail4@users.noreply.github.com>
2022-12-18 14:07:28 +0700

    Fix upload avatar  only when preserit

--- utils/uploadAvatar.js
+++ utils/uploadAvatar.js
@@ -1,14 +1,14 @@
 const sharp = require('sharp')
+const path  = require('path')
 const fs = require('fs')
-const path = require('path')
 
-module.exports = async(req,id)=>{
-    if (req.file) {
-        const avatarPath = `/uploads/${id}.jpg`
-        await sharp(req.file.path).resize(240,240)
-        .jpeg({quality:70})
-        .flatten({background:'#fff'})
-        .toFile(path.join(__dirname,`../public${avatarPath}`))
-        await fs.promises.unlink(req.file.path)
+module.exports = async(req, id)=>{
+    if(req.file){
+      const avatarPath = `/uploads/${id}.jpg`
+      await sharp(req.file.path).resize(200,200).jpeg({quality:70}).flatten({background:'#fff'})
+        .toFile(path.join(__dirname, `../public${avatarPath}`))
+      await fs.promises.unlink(req.file.path)
+      return avatarPath
     }
+
 }
